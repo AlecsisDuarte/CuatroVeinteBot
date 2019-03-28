32e6a45995fd50195a84d22955fcac638521c0a1
Alexis Duarte <alexis.duarte@hotmail.com>
2019-03-28 13:33:03 -0700

    Added comments to the functions

--- index.js
+++ index.js
@@ -5,10 +5,9 @@ const moment = require('moment-timezone');
 const bot = new Telebot(process.env.TELEGRAM_BOT_TOKEN);
 
 const GIF_TAGS = ['420', 'WEED', 'marijuana', 'blunt'];
-// const MAX_GIPHY_OFFSET = 100;
 const DEFAULT_TIMEZONE = 'America/Tijuana';
 
-const chatIds = [509707694];
+const chatIds = [];
 let lastMinute = 0;
 
 bot.on(['/420', '/start'], (msg) => {
@@ -18,13 +17,15 @@ bot.on(['/420', '/start'], (msg) => {
 
 bot.on('/un420', (msg) => {
     //   const chatId = msg.chat.id;
-    msg.reply.text(`You wont be able to un-420 your ass @${msg.from.username}`);
+    msg.reply.text(`You wont be able to un-420 @${msg.from.username}`);
     // removeChatFromChatsList(chatId);
 });
 
 bot.on('/timezone', (msg) => {
     const chatId = msg.chat.id;
-    bot.sendMessage(chatId, `The default timezone is **${DEFAULT_TIMEZONE}**`, { parseMode: 'Markdown' });
+    bot.sendMessage(chatId, `The default timezone is **${DEFAULT_TIMEZONE}**`, {
+        parseMode: 'Markdown'
+    });
 });
 
 bot.on('tick', (msg, dk, tick) => {
@@ -35,33 +36,42 @@ bot.on('tick', (msg, dk, tick) => {
 
 bot.start();
 
+/**
+ * Sends a random gif to all the chats in the [chatIds] list
+ * from Giphy using a random tag from the 
+ * list of tags stored in [GIF_TAGS]
+ */
 function sendFourTwentyGif() {
     try {
-        const tagIndex = Math.floor(Math.random() * (GIF_TAGS.length));
-        Giphy.random(
-            {
-                tag: GIF_TAGS[tagIndex],
-                fmt: 'json',
-            },
-            (err, res) => {
-                if (err) {
-                    console.error('Error', err);
-                    return;
-                }
+        for (const index in chatIds) {
+            const tagIndex = Math.floor(Math.random() * (GIF_TAGS.length));
+            Giphy.random({
+                    tag: GIF_TAGS[tagIndex],
+                    fmt: 'json',
+                },
+                (err, res) => {
+                    if (err) {
+                        console.error('Error', err);
+                        return;
+                    }
 
-                const gifURL = res.data.image_url;
-                const gifTag = GIF_TAGS[tagIndex];
-                for (const index in chatIds) {
+                    const gifURL = res.data.image_url;
+                    const gifTag = GIF_TAGS[tagIndex];
                     bot.sendDocument(chatIds[index], gifURL, {
                         caption: `Here is your 420 gif (#${gifTag} tag used). See you in 12 hours`,
                     });
-                };
-            });
+                });
+        };
     } catch (err) {
         console.error('sendFourTwentyGif', err);
     }
 }
 
+/**
+ * Validates whether the current time is 420
+ * using the default timezone
+ * @returns {boolean} True or False if it's 420
+ */
 function isFourTwenty() {
     try {
         const mnt = moment().tz(DEFAULT_TIMEZONE);
@@ -73,6 +83,12 @@ function isFourTwenty() {
     }
 }
 
+/**
+ * Validates whether a minute has passed or not
+ * using the global variable [lastMinute]
+ * @returns {boolean} True or False if a minute
+ * has passed
+ */
 function minuteLapsed() {
     try {
         const mnt = moment().tz(DEFAULT_TIMEZONE);
@@ -89,27 +105,43 @@ function minuteLapsed() {
     }
 }
 
+/**
+ * Adds a ChatId to the current List
+ * @param {number} chatId Id of the Chat to add
+ */
 function addChatToChatsList(chatId) {
     try {
         if (!chatIds.includes(chatId)) {
             chatIds.push(chatId);
-            bot.sendMessage(chatId, 'This chat has been **addedd** to my __420 chats__ list', { parseMode: 'Markdown' });
+            bot.sendMessage(chatId, 'This chat has been **addedd** to my __420 chats__ list', {
+                parseMode: 'Markdown'
+            });
         } else {
-            bot.sendMessage(chatId, 'This chat **already existsd** in my __420 chats__ list', { parseMode: 'Markdown' });
+            bot.sendMessage(chatId, 'This chat **already existsd** in my __420 chats__ list', {
+                parseMode: 'Markdown'
+            });
         }
     } catch (err) {
         console.error('addChatToChatsList', err);
     }
 }
 
+/**
+ * Removes the ChatId from the list of chats
+ * @param {number} chatId Id of the Chat to remove
+ */
 function removeChatFromChatsList(chatId) {
     let indexOfChatId = -1;
     try {
         if ((indexOfChatId = chatIds.indexOf(chatId)) >= 0) {
             chatIds.splice(indexOfChatId, 1);
-            bot.sendMessage(chatId, 'The chat has been **removedd** from my __420 chats__ list', { parseMode: 'Markdown' });
+            bot.sendMessage(chatId, 'The chat has been **removedd** from my __420 chats__ list', {
+                parseMode: 'Markdown'
+            });
         } else {
-            bot.sendMessage(chatId, 'This chat **doesn\'t existd** in my __420 chats__ list', { parseMode: 'Markdown' });
+            bot.sendMessage(chatId, 'This chat **doesn\'t existd** in my __420 chats__ list', {
+                parseMode: 'Markdown'
+            });
         }
     } catch (err) {
         console.error('removeChatFromChatsList', err);
